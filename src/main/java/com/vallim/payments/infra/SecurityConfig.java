46e988f2142d4e554daeabfa043e91d1efa243ba
Fabricio Vallim <fmvallim@gmail.com>
2024-07-07 21:05:01 -0300

    Fixes

--- src/main/java/com/vallim/payments/infra/SecurityConfig.java
+++ src/main/java/com/vallim/payments/infra/SecurityConfig.java
@@ -9,17 +9,13 @@ public class SecurityConfig {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/payments", "/webhooks", "/api-mock/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**");
+        return (web) -> web.ignoring().requestMatchers(
+                "/payments",
+                "/webhooks",
+                "/api-mock/**",
+                "/v3/api-docs/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**");
     }
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf().disable()
-//                .authorizeHttpRequests(a -> a
-//                        .requestMatchers("/v3/**", "/swagger-ui/**").permitAll()
-//                        .anyRequest().authenticated()
-//                ).build();
-//    }
 
 }
